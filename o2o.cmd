8ed842fc52da27b1c1342f884c3374f2703625a8
SayCV <sssxyc@163.com>
2014-11-08 16:21:01 +0800

    ...

--- o2o.cmd
+++ o2o.cmd
@@ -2,6 +2,11 @@
 title %~n0
 cd /d %~dp0
 
+set "CHROME_TOP_ROOT_XP=%userprofile%/AppData/Local/Chromium"
+set "CHROME_TOP_ROOT_WIN7=%userprofile%/Local Settings/Application Data/Chromium"
+set "PATH=%CHROME_TOP_ROOT_XP%/Application;%PATH%"
+set "PATH=%CHROME_TOP_ROOT_WIN7%/Application;%PATH%"
+
 set "ANDROID_TOP_ROOT=D:/Android"
 set "ANDROID_SDK_TOP_ROOT=D:/Android/android-studio/sdk"
 
@@ -10,6 +15,16 @@ set "NODEJS_ROOT=%NODEJS_TOP_ROOT%"
 set "NODEJS_HOME=%NODEJS_TOP_ROOT%"
 set "PATH=%NODEJS_ROOT%;%PATH%"
 rem call %NODEJS_HOME%/nodevars.bat
+
+set "URL=http://localhost:3000"
+
+if not exist %CHROME_TOP_ROOT_XP% (
+	if not exist %CHROME_TOP_ROOT_WIN7% (
+		start /min iexplore "%URL%"
+	)
+) else (
+	start /min chrome "%URL%"
+)
 
 node server.js
 
